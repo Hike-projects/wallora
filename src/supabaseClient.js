48f9ca380f0d4484d5afda12e76862bd3a673b25
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-02-14 16:05:23 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Replace with your actual Supabase Project URL and Public Anon Key
-const SUPABASE_URL = 'https://rptdmaistscgifwhnkzg.supabase.co'; // Your Project URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInT5cCI6IkpXVCJ9......'; // Your anon key
+// Project URL and Secret Key from your Supabase project
+const supabaseUrl = 'https://rptdmaistscgifwhnkzg.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwdGRtYWlzdHNjZ2lmd2hua3pnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczOTUwNTgzMCwiZXhwIjoyMDU1MDgxODMwfQ.cOhB6k7TkGbV8zX6vHXVMSHv1FePaFY1v1uDC0ykFK4';
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
