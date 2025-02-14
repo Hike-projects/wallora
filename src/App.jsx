b80df7c38e7cf82c0f5b17b758fbf67c659ea475
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-02-14 09:54:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-// src/App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import WallpaperGallery from "./pages/WallpaperGallery";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'; // Check this carefully
+import WallpaperGallery from './pages/WallpaperGallery'; // Check this too
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Route for Home page */}
         <Route path="/" element={<Home />} />
-
-        {/* Route for Wallpaper Gallery page */}
         <Route path="/gallery" element={<WallpaperGallery />} />
       </Routes>
     </Router>
